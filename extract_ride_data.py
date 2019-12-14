cb4435e682c91c07216e548a8c18dad366b7d4e1
Brian T. Rice <briantrice@gmail.com>
2019-12-14 11:24:42 -0800

    reformat and clean up, using an event_type parser

--- extract_ride_data.py
+++ extract_ride_data.py
@@ -2,24 +2,24 @@
 
 import os
 from datetime import datetime
-import argparse
+import re
 import json
 
 
 class LogHeader():
-    def __init__(self, log_file):
-        self.log_title = log_file.readline()
-        log_file.readline()
-        self.serial_no = log_file.readline()
-        self.vin = log_file.readline()
-        self.firmware_rev = log_file.readline()
-        self.board_rev = log_file.readline()
-        self.model = log_file.readline()
-        log_file.readline()
-        self.log_entries_count = log_file.readline()
-        log_file.readline()
-        self.column_headings = log_file.readline()
-        self.column_divider = log_file.readline()
+    def __init__(self, log_input_file):
+        self.log_title = log_input_file.readline()
+        log_input_file.readline()
+        self.serial_no = log_input_file.readline()
+        self.vin = log_input_file.readline()
+        self.firmware_rev = log_input_file.readline()
+        self.board_rev = log_input_file.readline()
+        self.model = log_input_file.readline()
+        log_input_file.readline()
+        self.log_entries_count = log_input_file.readline()
+        log_input_file.readline()
+        self.column_headings = log_input_file.readline()
+        self.column_divider = log_input_file.readline()
 
 
 class LogEntry():
@@ -32,22 +32,53 @@ class LogEntry():
                     self.timestamp = datetime.strptime(timestamp_text, '%m/%d/%Y %H:%M:%S')
                 except ValueError:
                     self.timestamp = None
-            self.event = log_text[33:].strip()
-            self.conditions = None
+            message = log_text[33:].strip()
+            [self.event_type, self.event] = self.type_and_event_from_message(message)
             if self.has_conditions():
                 self.event = log_text[33:59].strip()
                 self.conditions = log_text[60:].strip()
+            else:
+                self.conditions = None
         except Exception as e:
             print(log_text)
             raise e
 
+    @classmethod
+    def type_and_event_from_message(cls, message: str) -> [str]:
+        event_type = ''
+        event_contents = message
+        if message.startswith('0x'):
+            event_type = 'BINARY'
+        elif re.match('^[A-Z][a-z]+ing', message):
+            event_type = message.split(' ')[0].upper()
+            event_contents = ''
+        elif message.endswith(' Connected'):
+            event_type = 'CONNECTED'
+        elif message.endswith(' Disconnected'):
+            event_type = 'DISCONNECTED'
+        elif message.startswith('INFO:'):
+            event_type = 'INFO'
+            event_contents = message[6:]
+        elif message.startswith('DEBUG:'):
+            event_type = 'DEBUG'
+            event_contents = message[7:]
+        elif message.startswith('WARNING:'):
+            event_type = 'WARNING'
+            event_contents = message[8:]
+        elif message.endswith(' On') or ' On ' in message:
+            event_type = 'ON'
+            event_contents = message[:-3]
+        elif message.endswith(' Off') or ' Off ' in message:
+            event_type = 'OFF'
+            event_contents = message[:-4]
+        return [event_type, event_contents]
+
     def is_notice(self):
-        return (self.event.startswith('INFO:') or
-                self.event.startswith('DEBUG:') or
-                self.event.startswith('WARNING:'))
+        return self.event_type in ['INFO', 'DEBUG', 'WARNING']
 
     def has_conditions(self):
-        return not self.is_notice()
+        return (not self.is_notice() and
+                self.event_type not in ['BINARY', 'CONNECTED', 'DISCONNECTED'])
 
     def is_ride_entry(self):
         return self.entry == 'Riding'
@@ -59,6 +90,7 @@ class LogEntry():
         return sep.join([
             str(self.entry),
             self.timestamp and str(self.timestamp) or '',
+            self.event_type,
             self.event,
             self.conditions and self.conditions or ''])
 
@@ -69,17 +101,23 @@ class LogEntry():
         return json.dumps({
             'entry': self.entry,
             'timestamp': self.timestamp and str(self.timestamp) or '',
+            'event_type': self.event_type,
             'event': self.event,
             'conditions': self.conditions
         })
 
 
+tabular_headers = ['Entry', 'Timestamp', 'Type', 'Event', 'Conditions']
+
 if __name__ == "__main__":
+    import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("--format", default='tsv',
                         help="the output format desired: csv,tsv,json")
     parser.add_argument("logfile",
                         help="the parsed log file to process")
+    parser.add_argument("--outfile",
+                        help="the name of output file to emit")
 
     args = parser.parse_args()
     log_filepath = args.logfile
@@ -88,11 +126,22 @@ if __name__ == "__main__":
         exit(1)
     output_format = args.format
     line_sep = os.linesep
+    output_filepath = args.outfile
+    if not output_filepath:
+        output_filepath = log_filepath + '.' + output_format
 
     with open(log_filepath) as log_file:
+        # Read header:
         header_info = LogHeader(log_file)
-        with open("output.csv", 'w') as output:
+        with open(output_filepath, 'w') as output:
+            # Read log entries:
             line = log_file.readline()
+            # Write header:
+            if output_format == 'csv':
+                output.write(','.join(tabular_headers) + line_sep)
+            elif output_format == 'tsv':
+                output.write('\t'.join(tabular_headers) + line_sep)
+            # Process log entries:
             while line:
                 log_entry = LogEntry(line)
                 if output_format == "csv":
