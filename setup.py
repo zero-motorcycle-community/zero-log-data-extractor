73f1103681811e0f5b10f3618b6c8f6dd1d20163
Brian T. Rice <briantrice@gmail.com>
2019-12-17 15:10:14 -0800

    add a package setup script

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='Zero log data extractor',
+      version='0.1',
+      description='Extract tabular data from parsed/text Zero motorcycle log files',
+      url='https://github.com/zero-motorcycle-community/zero-log-data-extractor',
+      author='Brian T. Rice',
+      author_email='briantrice@gmail.com',
+      license='CC0 1.0',
+      packages=[],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      zip_safe=False)
